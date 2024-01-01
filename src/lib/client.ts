8fbb10795c1b44e11cb27a6ce6427667ea8dd36e
yuma0306 <yuma0306895@gmail.com>
2024-01-01 18:38:22 +0900

    サンプルコメントアウト

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,6 @@
-import { createClient } from 'microcms-js-sdk';
+// import { createClient } from 'microcms-js-sdk';
 
-export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN, //　XXXX.microcms.io の XXXX 部分
-  apiKey: process.env.API_KEY,
-});
+// export const client = createClient({
+//   serviceDomain: process.env.SERVICE_DOMAIN, //　XXXX.microcms.io の XXXX 部分
+//   apiKey: process.env.API_KEY,
+// });
