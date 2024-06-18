5a9148ac437793765d624b6e571d4299f0f26927
yuma0306 <yuma0306895@gmail.com>
2024-06-18 20:30:57 +0900

    fix copy

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='py-4 px-5'>
       <Inner>
-        <p className='text-center'>© 2023 Uma Code</p>
+        <p className='text-center'>© Uma Code</p>
       </Inner>
     </footer>
   )
