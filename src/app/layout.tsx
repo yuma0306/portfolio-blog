5f937ada33b2e53735661d642d100f7d45dba2ad
yuma0306 <yuma0306895@gmail.com>
2024-06-18 21:26:58 +0900

    fix ogp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,23 @@ export const metadata: Metadata = {
   description: '自主制作したポートフォリオを紹介しています。 | Uma Code',
   robots: {
     index: false,
-  }
+  },
+  openGraph: {
+    title: 'Uma Code',
+    description: '自主制作したポートフォリオを紹介しています。 | Uma Code',
+    url: `${process.env.VERCEL_URL ?? 'http://localhost:3000'}`,
+    siteName: 'Uma Code',
+    images: [
+      {
+        url: `${process.env.VERCEL_URL ?? 'http://localhost:3000'}/ogp.png`,
+        width: 1200,
+        height: 630,
+        alt: 'OGP画像',
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
